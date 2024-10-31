cd8e4f87f5a9dd4099d77159e2ccda2ce42ef3e7
Bits, Bytes, and Bourbon <sean.kalinich@decryptedtech.com>
2024-10-31 10:58:35 -0400

    Create Require_NTLMv2.ps1
    With vulnerabilities in NTLMv1, requiring the use of NTLMv2 helps remove another low hanging fruit from attacker's reach.
    This simple script checks the registry and and changed the LmCompatibilityLevel Key to 5 which enables the following.
    
    "Client devices use NTLMv2 authentication, and they use NTLMv2 session security if the server supports it. Domain controllers refuse to accept LM and NTLM authentication, and they'll accept only NTLMv2 authentication."
    
    Signed-off-by: Bits, Bytes, and Bourbon <sean.kalinich@decryptedtech.com>

---
+++ Require_NTLMv2.ps1
@@ -0,0 +1,21 @@
+# set your path, key, and value for get-property
+$RegistryPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa'
+$Name = 'LmCompatibilityLevel'
+$Value = '5'
+#output and get-property statement
+$write = 'NTLM Version 2 Already Set'
+$Property = (Get-ItemPropertyValue -Path $RegistryPath -Name $Name)
+#Function to check the value you are looking For and enter if absent
+Function Test-RegistryValue ($RegsitryPath, $Name, $Value) {
+$RegistryPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa'
+$Name = 'LmCompatibilityLevel'
+$Value = '5'
+	if ($null -ne $Property) {
+		Write-Output "$write"
+		}
+	else {
+		New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD -Force
+	}
+}
+#run the funciton
+Test-RegistryValue
