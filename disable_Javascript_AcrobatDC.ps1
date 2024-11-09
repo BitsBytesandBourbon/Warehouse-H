e7248db2f839fe14d8a1e157bf6aa70c91c325a1
Bits, Bytes, and Bourbon <sean.kalinich@decryptedtech.com>
2024-11-09 11:00:20 -0500

    disable_Javascript_AcrobatDC.ps1
    Added additional logic and error handling
    
    Signed-off-by: Bits, Bytes, and Bourbon <sean.kalinich@decryptedtech.com>

--- disable_Javascript_AcrobatDC.ps1
+++ disable_Javascript_AcrobatDC.ps1
@@ -4,13 +4,14 @@ $Name = 'bDisableJavaScript'
 $Value = '1'
 #output and value check statements
 $write = 'JavaScript is Disbaled for Acrobat DC'
-$Property = (Get-ItemPropertyValue -Path $RegistryPath).$Name
+$Property = (Get-ItemProperty -Path $RegistryPath -Name $Name -ErrorAction Ignore)
+$Property1 = (Get-ItemPropertyValue -Path $RegistryPath -Name $Name -ErrorAction Ignore)
 #Function to check the value you are looking For and enter if absent
 Function Test-RegistryValue ($RegsitryPath, $Name, $Value) {
 $RegistryPath = 'HKLM:SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\'
 $Name = 'bDisableJavaScript'
 $Value = '1'
-	if ($Property eq 1) {
+	if (($Property -like '*bDisableJavaScript*') -and ($Property1 -match '^1$')) {
 		Write-Output "$write"
 		}
 	else {
