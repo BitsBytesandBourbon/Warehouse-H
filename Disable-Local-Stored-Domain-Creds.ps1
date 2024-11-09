e0ed03a8599ee8e2b84971e6d418d70d74e22386
Bits, Bytes, and Bourbon <sean.kalinich@decryptedtech.com>
2024-11-09 14:43:45 -0500

    Update Disable-Local-Stored-Domain-Creds.ps1
    Added error handling in the logic
    
    Signed-off-by: Bits, Bytes, and Bourbon <sean.kalinich@decryptedtech.com>

--- Disable-Local-Stored-Domain-Creds.ps1
+++ Disable-Local-Stored-Domain-Creds.ps1
@@ -1,10 +1,11 @@
-# set your path, key, and value here for the propery grab
-$RegistryPath = 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa'
+# set your path, key, and value here
+$RegistryPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\'
 $Name = 'DisableDomainCreds'
 $Value = '1'
-#output and property vairable setting
+#output and value check statements
 $write = 'Already Set Properly'
-$Property = (Get-ItemProperty -Path $RegistryPath -Name $Name)
+$ErrorActionPreference = "silentlycontinue"
+$Property = (Get-ItemPropertyValue -Path $RegistryPath -Name $Name -ErrorAction SilentlyContinue)
 #Function to check the value you are looking For and enter if absent
 Function Test-RegistryValue ($RegsitryPath, $Name, $Value) {
 $RegistryPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\'
