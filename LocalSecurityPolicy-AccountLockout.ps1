d14c4b849cf20a1166691bfa8019a6270e5846fe
Bits, Bytes, and Bourbon <sean.kalinich@decryptedtech.com>
2024-11-10 17:21:41 -0500

    Update LocalSecurityPolicy-AccountLockout.ps1
    Changed lockout setting to 5 attempts
    
    Signed-off-by: Bits, Bytes, and Bourbon <sean.kalinich@decryptedtech.com>

--- LocalSecurityPolicy-AccountLockout.ps1
+++ LocalSecurityPolicy-AccountLockout.ps1
@@ -1,5 +1,5 @@
 secedit /export /cfg $env:TEMP\security.inf
-(Get-Content $env:TEMP\security.inf -Raw) -Replace '\bLockoutBadCount\s*=\s*(?:[1-9]|1[1-9])\b','LockoutBadCount = 10' | Set-Content "$env:TEMP\security.inf"
+(Get-Content $env:TEMP\security.inf -Raw) -Replace '\bLockoutBadCount\s*=\s*(?:[0-4]|[6-9]|1[0-9])\b','LockoutBadCount = 5' | Set-Content "$env:TEMP\security.inf"
 (Get-Content $env:TEMP\security.inf -Raw) -Replace '\bResetLockoutCount\s*=\s*(?:[1-9]|1[0-4]|1[6-9])\b','ResetLockoutCount = 15' | Set-Content "$env:TEMP\security.inf"
 (Get-Content $env:TEMP\security.inf -Raw) -Replace '\bLockoutDuration\s=\s*(?:[1-9]|1[0-4]|[2-9]\d|1[6-9])\b','LockoutDuration = 15' | Set-Content "$env:TEMP\security.inf"
 secedit /import /cfg "$env:TEMP\security.inf" /db "c:\windows\security\database\local.sdb" /areas SECURITYPOLICY 
