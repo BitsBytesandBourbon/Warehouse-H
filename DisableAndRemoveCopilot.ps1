8e3fe450ecea57298a493914015060fa8dcf177b
Bits, Bytes, and Bourbon <sean.kalinich@decryptedtech.com>
2025-12-03 16:41:51 -0500

    Create DisableAndRemoveCopilot.ps1
    A fun Script to find Copilot in the registry and disable it there, it also removes the Appx Packages related to Copilot
    
    Signed-off-by: Bits, Bytes, and Bourbon <sean.kalinich@decryptedtech.com>

---
+++ DisableAndRemoveCopilot.ps1
@@ -0,0 +1,51 @@
+$user = (Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty Username) 
+$sid = (New-Object System.Security.Principal.NTAccount($user)).Translate([System.Security.Principal.SecurityIdentifier]).Value
+New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS
+
+#Function to check the value you are looking For and enter if absent
+Function Test-RegistryValue1 ($RegsitryPath1, $Name1) {
+$RegistryPath1 = "HKU:\$sid\Software\Policies\Microsoft\Windows\WindowsCopilot"
+$Name1 = "WindowsCopilot"
+$Property1 = (Get-Item -Path $RegistryPath1).PSChildName
+	if ( $Property1 -eq $Name1 ) {
+		Write-Output 'Key Already Exists'
+			
+		}
+	else {
+		New-Item -Path $RegistryPath1 -Name $Name1 -Force
+	}
+}
+#run funciton1
+Test-RegistryValue1
+
+#Function to check the value you are looking For and enter if absent
+Function Test-RegistryValue2 ($RegsitryPath2, $Name2, $Value2) {
+$RegistryPath2 = "HKU:\$sid\Software\Policies\Microsoft\Windows\WindowsCopilot"
+$Name2 = "TurnOffWindowsCopilot"
+$Value2 = "1"
+$Property2 = (Get-ItemPropertyValue -Path $RegistryPath2 -Name $Name2)
+	if ($Property2 -ne 1) {
+		New-ItemProperty -Path $RegistryPath2 -Name $Name2 -Value $Value2 -PropertyType DWORD -Force
+		Write-Host 'Checking for Copilot AppxPacakges'
+		$CopilotLives = (Get-AppxPackage -AllUsers | Where-Object {$_.Name -like "*Copilot*"})
+		If ($Null -ne $CopilotLives) {
+		Write-Host 'Uninstalling Copilot AppxPackages'
+		Get-AppxPackage -AllUsers | Where-Object {$_.Name -like "*Copilot*"} | ForEach-Object {Remove-AppxPackage -Package $_.PackageFullName -AllUsers -ErrorAction SilentlyContinue} }
+		else {
+		Write-Host 'Copilot Uninstalled'	
+	}
+	}
+	else {
+		Write-Host 'Copilot Already Disabled, Now Unisntalling Copilot App'	
+		Write-Host 'Checking for Copilot AppxPacakges'
+		$CopilotLives = (Get-AppxPackage -AllUsers | Where-Object {$_.Name -like "*Copilot*"})
+		If ($Null -ne $CopilotLives) {
+		Write-Host 'Uninstalling Copilot AppxPackages'
+		Get-AppxPackage -AllUsers | Where-Object {$_.Name -like "*Copilot*"} | ForEach-Object {Remove-AppxPackage -Package $_.PackageFullName -AllUsers -ErrorAction SilentlyContinue} }
+		else {
+		Write-Host 'Copilot Uninstalled'	
+	}
+	}
+}
+#run funciton2 
+Test-RegistryValue2
