99da690a789a7088c4aa66f1c9eeb14b91ed03fd
Bits, Bytes, and Bourbon <sean.kalinich@decryptedtech.com>
2025-12-03 13:43:12 -0500

    Update UnifiedAuditLoggingCheck.ps1
    Signed-off-by: Bits, Bytes, and Bourbon <sean.kalinich@decryptedtech.com>

--- UnifiedAuditLoggingCheck.ps1
+++ UnifiedAuditLoggingCheck.ps1
@@ -10,15 +10,20 @@ Wait-Job -Name Install
 Write-Output "Exchange Online Management Installed"
 
 #enter the UPN for the tenant you are acccessig
-Connect-ExchangeOnline -UserPrincipalName <Insert UPN Here> -ShowBanner:$false
+Write-Output "Logging In"
+Connect-ExchangeOnline -UserPrincipalName sk2715@decryptedtech.com -ShowBanner:$false
 #function to check to see if Unified Audit Logging is enabled and to enable it if not
 Function Check_UnifiedLogging ($AuditlogState) {
+	Write-Output "Checking Unified Log Status"
 $AuditlogState = (Get-AdminAuditLogConfig).UnifiedAuditLogIngestionEnabled
 	If ($AuditlogState -eq "True") {
 		Write-Output 'Congrats, you have unified Audit Logs already Enabeld'	
 		}
 	else {
-		Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled:$true
+		Write-Output "Enabling Unified Audit Logs"
+		Start-Job -Name SetAuditLogs -ScriptBlock {Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled:$true}
+		Wait-Job -Name SetAuditLogs
+		Write-Output "Unified Audit Logs Enabled"
 	}
 }
 #run the function
